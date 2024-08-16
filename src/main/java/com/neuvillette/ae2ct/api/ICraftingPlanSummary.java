3f3f99ce67b83a1aafd1ed5cef92b47c256c5a12
lumine <463305152@qq.com>
2024-08-16 22:18:58 +0800

    network

--- src/main/java/com/neuvillette/ae2ct/api/ICraftingPlanSummary.java
+++ src/main/java/com/neuvillette/ae2ct/api/ICraftingPlanSummary.java
@@ -1,8 +1,9 @@
 package com.neuvillette.ae2ct.api;
 
 import appeng.api.networking.crafting.ICraftingPlan;
+import appeng.crafting.CraftingPlan;
 
 public interface ICraftingPlanSummary {
-    public ICraftingPlan getJob();
-    public void setJob(ICraftingPlan job);
+    public CraftingPlan getJob();
+    public void setJob(CraftingPlan job);
 }
--- src/main/java/com/neuvillette/ae2ct/mixin/AE2CraftingPlanSummary.java
+++ src/main/java/com/neuvillette/ae2ct/mixin/AE2CraftingPlanSummary.java
@@ -6,30 +6,44 @@ import appeng.api.networking.security.IActionSource;
 import appeng.crafting.CraftingPlan;
 import appeng.menu.me.crafting.CraftingPlanSummary;
 import com.neuvillette.ae2ct.api.ICraftingPlanSummary;
+import net.minecraft.network.RegistryFriendlyByteBuf;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(CraftingPlanSummary.class)
 public class AE2CraftingPlanSummary implements ICraftingPlanSummary {
     @Unique
-    private ICraftingPlan jobs;
+    private CraftingPlan jobs;
     @Inject(at = @At("TAIL"), method = "fromJob", cancellable = true)
     private static void buildEX(IGrid grid, IActionSource actionSource, ICraftingPlan job, CallbackInfoReturnable<CraftingPlanSummary> cir){
         var r = cir.getReturnValue();
-        ((ICraftingPlanSummary)r).setJob(job);
+        ((ICraftingPlanSummary)r).setJob((CraftingPlan) job);
         cir.setReturnValue(r);
     }
 
+    @Inject(at = @At("TAIL"), method = "write")
+    private void write(RegistryFriendlyByteBuf buffer, CallbackInfo ci){
+        buffer.writeInt(jobs.patternTimes().size());
+    }
+
+    @Inject(at = @At("TAIL"), method = "read", cancellable = true)
+    private static void read(RegistryFriendlyByteBuf buffer, CallbackInfoReturnable<CraftingPlanSummary> cir){
+        var r = cir.getReturnValue();
+        ((ICraftingPlanSummary)r).setJob((new CraftingPlan(null, buffer.readInt(), false, false, null, null, null, null)));
+
+    }
+
     @Override
-    public ICraftingPlan getJob(){
+    public CraftingPlan getJob(){
         return jobs;
     }
 
     @Override
-    public void setJob(ICraftingPlan job) {
+    public void setJob(CraftingPlan job) {
         this.jobs = job;
     }
 
