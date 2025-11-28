82e1d4b2168a2c905def63dc24921a70df2a3284
vcwdfca <2168409010@qq.com>
2025-11-28 13:08:03 +0800

    Fix the bug caused by clicking too quickly leading to out-of-bounds

--- src/main/java/com/neuvillette/ae2ct/gui/CraftingTreeWidget.java
+++ src/main/java/com/neuvillette/ae2ct/gui/CraftingTreeWidget.java
@@ -74,13 +74,13 @@ public class CraftingTreeWidget {
         PoseStack poseStack = guiGraphics.pose();
         poseStack.pushPose();
         updateNodeManager();
-        updateSearch();
         if(_nodeManager != null) {
             if (Config.USE_COMPACT_TREE.get() != helper.now_mode) {
                 helper.buildNodePosition(_nodeManager.root, _nodeManager);
             }
             poseStack.scale(scroll, scroll, scroll);
             drawNode(guiGraphics, _nodeManager.root);
+            updateSearch();
         } else {
             return;
         }
@@ -379,14 +379,17 @@ public class CraftingTreeWidget {
             currentMatchNode = null;
             return;
         }
-        currentMatchNode = cache.get(currentMatchIdx % cache.size());
+        currentMatchNode = cache.get(currentMatchIdx);
     }
 
     // Buttons' updatePosition
     public void matchSwitch(boolean next) {
         if(cache == null || cache.isEmpty()) return;
-        currentMatchIdx += (next ? 1 : -1);
-        currentMatchNode = cache.get(currentMatchIdx % cache.size());
+        currentMatchIdx += next ? 1 : -1;
+        while(currentMatchIdx < 0 || currentMatchIdx >= cache.size()) {
+            currentMatchIdx = (currentMatchIdx + cache.size()) % cache.size();
+        }
+        updateSearch();
         outputX = 20 - currentMatchNode.point.x * spacingX;
         outputY = 30 - currentMatchNode.point.y * spacingY;
     }
